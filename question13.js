d554c61431fa64c82bdd70b5b85432ee2e7c1f84
Ananthika <ananthikaspillai@gmail.com>
2023-07-12 19:32:28 +0530

    changed

--- question13.js
+++ question13.js
@@ -1,6 +1,4 @@
 let str='This is a test statement';
-
-
 for (let i=0;i<str.length;i++)
 {
 if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='=u')
---
+++ question17.js
@@ -0,0 +1,6 @@
+const char=['code','react','html','css','coder'];
+for(let i=0;i<char.length;i++)
+{
+if(char[i].length%2==0)  
+console.log(char[i]);
+}
